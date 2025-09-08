fd541a6682073bfd8571c9d6b2147af0d98a8d60
Rohit kumar kumawat <rohitkumawatrk2024@gmail.com>
2025-09-08 17:58:05 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://backend1-qkow.onrender.com/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://shop-back-tacx.onrender.com/api'
 })
 
 export default api
